37c1446e869f39dccf12e34d1d609974bd44d60d
serhiy pikulya <info@webguide.com.ua>
2021-11-05 19:45:30 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//let number = 5;
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = prompt('Xb dbgjdybkjcm dfv 18?', '18');
+// console.log(answer);
+
+// const answers = [];
+// answers[0] = prompt('Як вас звати?', '');
+// answers[1] = prompt('Як прізвище?', '');
+// answers[2] = prompt('Років?', '');
+
+// document.write(answers);
+
