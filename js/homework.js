4ccc0cce6246a3c5594c17486f9bac8d21fa4953
serhiy pikulya <info@webguide.com.ua>
2021-11-07 15:08:13 +0200

    homework 3

--- js/homework.js
+++ js/homework.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const numberOfFilms = +prompt("Скільки фільмів ви переглянули?", "");
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt("Скільки фільмів ви переглянули?", "");
+
+  while (numberOfFilms == null || numberOfFilms == '' || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Скільки фільмів ви переглянули?", "");
+  }
+}
+
+start();
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -9,6 +19,53 @@ const personalMovieDB = {
   genres: [],
   privat: false
 };
+
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const lastFilms = prompt("Останній переглянутий фільм?", ""),
+          ratingFilms = prompt("Яку йому оцінку поставите?", "");
+    if (lastFilms != null && ratingFilms != null && lastFilms != '' && ratingFilms != '' && lastFilms.length < 50) {
+      personalMovieDB.movies[lastFilms] = ratingFilms;
+    } else {
+      i--;
+    }
+  }  
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {console.log('Переглянуто мало фільмів');}
+  else if (personalMovieDB.count > 30) {console.log('Ви кіноман');}
+  else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('Ви класичний глядач');
+  } else {console.log('Відбулася помилка');}  
+}
+
+detectPersonalLevel();
+
+//ПРАКТИЧНЕ 3 - мій розв'язок
+// function showMyDB() {
+//   if (personalMovieDB.privat === false) console.log(personalMovieDB);
+// }
+
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+
+//showMyDB(personalMovieDB.privat); //Виклик значення приватності каталогу з БД
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    //const j = prompt(`Ваш улюблений жанр під номером ${i}?`, "");
+    personalMovieDB.genres[i-1] = prompt(`Ваш улюблений жанр під номером ${i}?`, "");
+  }
+}
+
+showMyDB();
+console.log(personalMovieDB);
 
 
 //ПРАКТИЧНЕ 1
@@ -40,22 +97,3 @@ const personalMovieDB = {
 //   personalMovieDB.movies[lastFilms] = ratingFilms;
 //   i++;
 // }
-
-for (let i = 0; i < 2; i++) {
-  const lastFilms = prompt("Останній переглянутий фільм?", ""),
-        ratingFilms = prompt("Яку йому оцінку поставите?", "");
-  if (lastFilms != null && ratingFilms != null && lastFilms != '' && ratingFilms != '' && lastFilms.length < 50) {
-    personalMovieDB.movies[lastFilms] = ratingFilms;
-  } else {
-    i--;
-  }
-}
-
-
-if (personalMovieDB.count < 10) {console.log('Переглянуто мало фільмів');}
-else if (personalMovieDB.count > 30) {console.log('Ви кіноман');}
-else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-  console.log('Ви класичний глядач');
-} else {console.log('Відбулася помилка');}
-
-console.log(personalMovieDB);
