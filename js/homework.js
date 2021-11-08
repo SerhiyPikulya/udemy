8ecf3b9fc1aad0eaefe9f48d90cbf872642d26db
serhiy pikulya <info@webguide.com.ua>
2021-11-08 19:34:26 +0200

    homework 4

--- js/homework.js
+++ js/homework.js
@@ -1,99 +1,78 @@
 "use strict";
 
-let numberOfFilms;
-
-function start() {
-  numberOfFilms = +prompt("Скільки фільмів ви переглянули?", "");
-
-  while (numberOfFilms == null || numberOfFilms == '' || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt("Скільки фільмів ви переглянули?", "");
-  }
-}
-
-start();
 
 const personalMovieDB = {
-  count: numberOfFilms,
+  count: 0,
   movies: {},
   actors: {},
   genres: [],
-  privat: false
+  privat: false,
+  start: function() {
+    personalMovieDB.count = +prompt("Скільки фільмів ви переглянули?", "");
+  
+    while (personalMovieDB.count == null || personalMovieDB.count == '' || isNaN(personalMovieDB.count)) {
+      personalMovieDB.count = +prompt("Скільки фільмів ви переглянули?", "");
+    }
+  },
+  rememberMyFilms: function() {
+    for (let i = 0; i < 2; i++) {
+      const lastFilms = prompt("Останній переглянутий фільм?", ""),
+            ratingFilms = prompt("Яку йому оцінку поставите?", "");
+      if (lastFilms != null && ratingFilms != null && lastFilms != '' && ratingFilms != '' && lastFilms.length < 50) {
+        personalMovieDB.movies[lastFilms] = ratingFilms;
+      } else {
+        i--;
+      }
+    }  
+  },
+  detectPersonalLevel: function() {
+    if (personalMovieDB.count < 10) {console.log('Переглянуто мало фільмів');}
+    else if (personalMovieDB.count > 30) {console.log('Ви кіноман');}
+    else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+      console.log('Ви класичний глядач');
+    } else {console.log('Відбулася помилка');}  
+  },
+  showMyDB: function(hidden) {
+    if (!hidden) {
+      console.log(personalMovieDB);
+    }
+  },
+  writeYourGenres: function() {
+    for (let i = 1; i <= 3; i++) {
+      personalMovieDB.genres[i-1] = prompt(`Ваш улюблений жанр під номером ${i}?`, "").toLowerCase();
+      if (personalMovieDB.genres[i-1] === null || personalMovieDB.genres[i-1] === '') {
+        i--;
+        console.log('Ви ввели некоректні дані');
+      }
+    }
+    // personalMovieDB.genres.forEach(function(item, i) {
+    //   console.log(`Улюблений жанр ${i+1} - це ${item}`);
+    // });
+    personalMovieDB.genres.forEach((item, i) => {
+      console.log(`Улюблений жанр ${i+1} - це ${item}`);
+    });
+  },
+  toggleVisibleMyDB: function() {
+    if (personalMovieDB.privat) {
+      personalMovieDB.privat = false;
+    } else {
+      personalMovieDB.privat = true;
+    }
+  }
 };
 
-function rememberMyFilms() {
-  for (let i = 0; i < 2; i++) {
-    const lastFilms = prompt("Останній переглянутий фільм?", ""),
-          ratingFilms = prompt("Яку йому оцінку поставите?", "");
-    if (lastFilms != null && ratingFilms != null && lastFilms != '' && ratingFilms != '' && lastFilms.length < 50) {
-      personalMovieDB.movies[lastFilms] = ratingFilms;
-    } else {
-      i--;
-    }
-  }  
-}
+//personalMovieDB.start();
+//personalMovieDB.rememberMyFilms();
+//personalMovieDB.detectPersonalLevel();
+personalMovieDB.writeYourGenres();
+// personalMovieDB.toggleVisibleMyDB();
+// personalMovieDB.showMyDB(personalMovieDB.privat);
 
-rememberMyFilms();
+//start();
+//rememberMyFilms();
+//detectPersonalLevel();
+//showMyDB(personalMovieDB.privat); //Показати БД, якщо значення приватності каталогу не falce
+//showMyDB();
+//writeYourGenres;
 
-function detectPersonalLevel() {
-  if (personalMovieDB.count < 10) {console.log('Переглянуто мало фільмів');}
-  else if (personalMovieDB.count > 30) {console.log('Ви кіноман');}
-  else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    console.log('Ви класичний глядач');
-  } else {console.log('Відбулася помилка');}  
-}
-
-detectPersonalLevel();
-
-//ПРАКТИЧНЕ 3 - мій розв'язок
-// function showMyDB() {
-//   if (personalMovieDB.privat === false) console.log(personalMovieDB);
-// }
-
-function showMyDB(hidden) {
-  if (!hidden) {
-    console.log(personalMovieDB);
-  }
-}
-
-//showMyDB(personalMovieDB.privat); //Виклик значення приватності каталогу з БД
-
-function writeYourGenres() {
-  for (let i = 1; i <= 3; i++) {
-    //const j = prompt(`Ваш улюблений жанр під номером ${i}?`, "");
-    personalMovieDB.genres[i-1] = prompt(`Ваш улюблений жанр під номером ${i}?`, "");
-  }
-}
-
-showMyDB();
 console.log(personalMovieDB);
-
-
-//ПРАКТИЧНЕ 1
-// const lastFilms = prompt("Останній переглянутий фільм?", ""),
-//       ratingFilms = prompt("Яку йому оцінку поставите?", ""),
-//       lastFilms2 = prompt("Останній переглянутий фільм?", ""),
-//       ratingFilms2 = prompt("Яку йому оцінку поставите?", "");
-
-// personalMovieDB.movies = {
-//   [lastFilms]: ratingFilms,
-//   [lastFilms2]: ratingFilms2
-// };
-
-// personalMovieDB.movies[lastFilms] = ratingFilms;
-// personalMovieDB.movies[lastFilms2] = ratingFilms2;
-
-//console.log(movies);
-
-//ПРАКТИЧНЕ 2 - мій розв'язок
-// let i = 1;
-// while (i <= 2) {
-//   let lastFilms = '',
-//       ratingFilms = '';
-//   do {
-//     lastFilms = prompt("Останній переглянутий фільм?", ""),
-//     ratingFilms = prompt("Яку йому оцінку поставите?", "");
-//   }
-//   while (lastFilms === null || lastFilms.length === 0 || lastFilms.length > 50);
-//   personalMovieDB.movies[lastFilms] = ratingFilms;
-//   i++;
-// }
--- js/script.js
+++ js/script.js
@@ -1,5 +1,41 @@
 "use strict";
 
+// !КОРИСНІ ПРИЙОМИ
+// console.log(Object.keys(`ім'я масиву`).length); //Отримати к-ть елементів масиву
+
+//arr.sort(compareNum)                            //сортування чисел в масиві
+// function compareNum(a, b) {
+//   return a - b;
+// } 
+
+// function copy(mainObj) {                       //Поверхове копіювання об'єктів, масивів
+//   let objCopy = {};
+
+//   for (let key in mainObj) {
+//     objCopy[key] = mainObj[key];
+//   }
+
+//   return objCopy;
+// }
+
+// const clone = Object.assign({}, add); //поверхове Копіювання/склеювання об'єктів - в пустий об додається об add
+
+//const newArray = oldArray.slice();              //копіювання Масивів
+
+// const arr1 = ['youtube', 'vimeo', 'tube'],        //об'єднання масивів, об'єктів через спред оператор ...
+//       arr2 = ['a', 'b', 'c'];
+//       arrNewSum = [...arr1, ...arr2, 'свої дані 1', 'cdj] lfys 2']
+
+// const newArr = [...oldArr];        //копіювання масивів, об'єктів через спред оператор ...
+// const newObj = {...oldObj};
+
+//Object.setPrototypeOf(o, proto); //Втановлення прототипу для об'єкта о - для існуючих об'єктів
+
+//const o = Object.create(proto);    //Створення нового об'єкту о з властивостями прототипу proto
+
+
+
+
 //let number = 5;
 // const result = confirm('Are you here?');
 // console.log(result);
@@ -14,3 +50,37 @@
 
 // document.write(answers);
 
+//ПРАКТИЧНЕ 1
+// const lastFilms = prompt("Останній переглянутий фільм?", ""),
+//       ratingFilms = prompt("Яку йому оцінку поставите?", ""),
+//       lastFilms2 = prompt("Останній переглянутий фільм?", ""),
+//       ratingFilms2 = prompt("Яку йому оцінку поставите?", "");
+
+// personalMovieDB.movies = {
+//   [lastFilms]: ratingFilms,
+//   [lastFilms2]: ratingFilms2
+// };
+
+// personalMovieDB.movies[lastFilms] = ratingFilms;
+// personalMovieDB.movies[lastFilms2] = ratingFilms2;
+
+//console.log(movies);
+
+//ПРАКТИЧНЕ 2 - мій розв'язок
+// let i = 1;
+// while (i <= 2) {
+//   let lastFilms = '',
+//       ratingFilms = '';
+//   do {
+//     lastFilms = prompt("Останній переглянутий фільм?", ""),
+//     ratingFilms = prompt("Яку йому оцінку поставите?", "");
+//   }
+//   while (lastFilms === null || lastFilms.length === 0 || lastFilms.length > 50);
+//   personalMovieDB.movies[lastFilms] = ratingFilms;
+//   i++;
+// }
+
+//ПРАКТИЧНЕ 3 - мій розв'язок
+// function showMyDB() {
+//   if (personalMovieDB.privat === false) console.log(personalMovieDB);
+// }
